b3f4e3ad9317ebca942b69107b91c0ead3830d94
Brad Langhorst <langhorst@neb.com>
2025-11-28 07:00:22 -0500

    keeps a local copy of svg files so differences can be easily observed.

--- scripts/flamegraph.sh
+++ scripts/flamegraph.sh
@@ -1,1 +1,2 @@
+mv flamegraph.svg flamegraph-$(date +%Y%m%d-%H%M%S).svg
 sudo CARGO_PROFILE_RELEASE_DEBUG=true RUST_LOG=info cargo flamegraph -- -i tests/fixtures/dnbseq-test1.bam -s 0 -e 100000000 --log-level ERROR > /dev/null 2>/dev/null
